fa01cf5b7ff34a2e8efb359fa5535a2778b6cd29
svitlana-sl <svitlana.slobodeniuk@gmail.com>
2025-03-26 11:34:53 +0100

    Add File uploader config with multer

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import chalk from "chalk";
 import { PORT } from "./utils/envs";
-
+import multer from "multer";
+import { log } from "console";
+import path from "path";
 // Variables
 const app = express();
 
@@ -11,8 +13,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("src/public"));
+app.use("/uploads", express.static("uploads"));
 
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: function (req, file, cb) {
+    const newName =
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    cb(null, newName);
+  },
+});
+const upload = multer({ storage: storage, limits: { fileSize: 3000000 } });
 // Routes
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    res.status(400).send("No file uploaded.");
+    return;
+  }
+  // const fileName = req.file.filename + "-" + Date.now() + path.extname(req.file.originalname);
+  const imageUrl = `/uploads/${req.file.filename}`;
+  res.status(200).send(`
+    <h1>Image uploaded successfully!</h1> 
+    <img src="http://localhost:3000${imageUrl}" width ="500"/>`);
+});
+
 app.get("/", (req, res) => {
   res.sendFile("/index.html");
 });
